de368cb9341077aff228c1aaa65e5b3efc6da933
MCBauer <mikecbauervision@gmail.com>
2020-05-18 15:00:44 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          My personal home page.
+        </p>
+        <ul>
+          Used to view my
+          <li>3d projects</li>
+          <li>React Projects</li>
+          <li>Traditional Art</li>
+          <li>My Favorite resources</li>
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
